dd212bf51926451c3976465a19e13107a1d20452
Igor Kuznetsov Home <im.best.man.of.planet@yandex.ru>
2024-01-13 13:20:39 +0300

    Code change for exercise 5.3

--- src/main/scala/DataFrame_5_step_3.scala
+++ src/main/scala/DataFrame_5_step_3.scala
@@ -1,23 +1,31 @@
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
 
+
+object DfColumn extends DfColumn {
+  implicit def columnToString(col: DfColumn.Value): String = col.toString
+}
+
+trait DfColumn extends Enumeration {
+  val _c0,
+  id, Word,
+  w_s1, w_s2,
+  cnt_s1, cnt_s2 = Value
+}
+
+
 object DataFrame_5_step_3 extends App {
 
   def filterHeader(df: DataFrame): DataFrame = {
     // Фильтруем данные от заголовков
     val words = "Game Of Thrones.*\\.srt"
-    df.filter(!col("_c0").rlike(words))
+    df.filter(!col(DfColumn._c0).rlike(words))
   }
 
   def filterNumberAndSpace(df: DataFrame): DataFrame = {
     // Фильтруем данные от чисел и пустых строк
-    val isSpace = (col("Word") =!= "") && col("Word").cast("int").isNull
+    val isSpace = (col(DfColumn.Word) =!= "") && col(DfColumn.Word).cast("int").isNull
     df.filter(isSpace)
-  }
-
-  def withLowerCase(df: DataFrame): DataFrame = {
-    // Преобразовываем слова в нижний регистр
-    df.withColumn("Word", lower(col("Word")))
   }
 
   def withSplitPhrase(df: DataFrame): DataFrame = {
@@ -25,35 +33,50 @@ object DataFrame_5_step_3 extends App {
      df
        .select(
             explode(
-              split(col("_c0"), "\\W+")
-            ).as("Word")
+              split(lower(col(DfColumn._c0)), "\\W+")
+            ).as(DfColumn.Word)
           )
   }
 
-  def withNumberId(df: DataFrame): DataFrame = {
+  def withId(df: DataFrame): DataFrame = {
     // Создаем столбец с номером строки
-    df.withColumn("id", monotonically_increasing_id())
+    df.withColumn(DfColumn.id, monotonically_increasing_id())
   }
 
-  def extractWordCount(col_word: String, col_count: String)(df: DataFrame): DataFrame = {
+  def extractWordCount(colWord: String, colCount: String)(df: DataFrame): DataFrame = {
     // Агрегируем слова, сортируем и забираем первые 20 строк
     df
-      .groupBy(col("Word").as(col_word))
-      .agg(count("Word").as(col_count))
-      .orderBy(desc(col_count))
+      .groupBy(col(DfColumn.Word).as(colWord))
+      .agg(count(DfColumn.Word).as(colCount))
+      .orderBy(desc(colCount))
       .limit(20)
   }
 
-  def pipeline(col_word: String = "Word",
-               col_count: String = "Total")(df: DataFrame): DataFrame = {
+  def pipeline(colWord: String = "Word",
+               colCount: String = "Total")(df: DataFrame): DataFrame = {
     // Общий поток преобразований
     df
       .transform(filterHeader)
       .transform(withSplitPhrase)
       .transform(filterNumberAndSpace)
-      .transform(withLowerCase)
-      .transform(extractWordCount(col_word, col_count))
-      .transform(withNumberId)
+      .transform(extractWordCount(colWord, colCount))
+      .transform(withId)
+  }
+
+  def joinDF(df2: DataFrame)(df1: DataFrame): DataFrame = {
+    // Условие для джойна
+    val joinCondition = df1.col(DfColumn.id) === df2.col(DfColumn.id)
+
+    df1
+      .join(df2, joinCondition, "inner")
+      .select("df1.id", DfColumn.w_s1, DfColumn.cnt_s1, DfColumn.w_s2, DfColumn.cnt_s2)
+  }
+
+  def readFile(file: String): DataFrame = {
+    spark
+      .read
+      .option("inferSchema", "true")
+      .csv(file)
   }
 
   val spark = SparkSession.builder()
@@ -61,24 +84,16 @@ object DataFrame_5_step_3 extends App {
     .config("spark.master", "local")
     .getOrCreate()
 
-  val sub_s1_DF = spark.read
-    .option("inferSchema", "true")
-    .csv("src/main/resources/subtitles_s1.json")
+  val subS1DF = readFile("src/main/resources/subtitles_s1.json")
+  val subS2DF = readFile("src/main/resources/subtitles_s2.json")
 
-  val sub_s2_DF = spark.read
-    .option("inferSchema", "true")
-    .csv("src/main/resources/subtitles_s2.json")
 
-  // Получаем агрегированые данные по каждому сезону
-  val agg_s1_DF = pipeline("w_s1", "cnt_s1")(sub_s1_DF).as("df1")
-  val agg_s2_DF = pipeline("w_s2", "cnt_s2")(sub_s2_DF).as("df2")
-
-  // Условие для джойна
-  val joinCondition = agg_s1_DF.col("id") === agg_s2_DF.col("id")
-
-  val resultDF = agg_s1_DF
-    .join(agg_s2_DF, joinCondition, "inner")
-    .select("df1.id", "w_s1", "cnt_s1", "w_s2", "cnt_s2")
+  val resultDF = pipeline("w_s1", "cnt_s1")(subS1DF).as("df1")
+    .transform(
+      joinDF(
+        pipeline("w_s2", "cnt_s2")(subS2DF).as("df2")
+      )
+    )
 
   resultDF.write
     .format("json")
