60a6c2fa33a93c93c80a094b088b547ed37bd62b
Igor Kuznetsov Home <im.best.man.of.planet@yandex.ru>
2024-01-24 10:09:02 +0300

    Code change for exercise DataSet 3.4 v2

--- src/main/scala/DataSet_3_step_4.scala
+++ src/main/scala/DataSet_3_step_4.scala
@@ -15,7 +15,18 @@ object DataSet_3_step_4 extends App with Context {
     df
       .withColumn("CompanyReviews", regexp_replace(col("CompanyReviews"), ",", "")
       )
-      .select()
+  }
+
+  def replaceNull(df: DataFrame): DataFrame = {
+    df
+      .na
+      .fill(
+        Map(
+          "JobTitle" -> "Unknown",
+          "Company" -> "Unknown",
+          "CompanyReviews" -> 0,
+        )
+      )
   }
 
   import spark.implicits._
@@ -23,21 +34,45 @@ object DataSet_3_step_4 extends App with Context {
     .read
     .option("header", "true")
     .option("multiLine", "true")
-    .option("escape", "\'")
-    .option("inferSchema", "true")
+    .option("escape", "\"")
+    .option("quote", "\"")
+//    .option("inferSchema", "true")
     .csv("src/main/resources/AIJobsIndustry.csv")
+
+
+  //"Internship: ""Data science"""
+  //""">【智通所】LTE網路研發工程師(定期契約)"
+  //"PhD Researcher ""Machine Learning for correct component design and manufacturing"""
+
+  val df1 = spark
+    .read
+    .option("lineSep", "\r")
+    .text("src/main/resources/AIJobsIndustry.csv")
+
+  df1.withColumn("value",
+      regexp_replace($"value", "\"\"\"\\w", "XXXXX")
+    )
+
+  df1.filter(col("value").rlike("\"\"\"\\w")).show()
+
+  df1.show()
+
 
   val cnt = df
     .transform(convert)
+//    .transform(replaceNull)
 
   cnt
     .show()
-//  df.printSchema()
-//
-//
-//  df.write
-//    .format("json")
-//    .mode(SaveMode.Overwrite)
-//    .save("src/main/resources/data/test")
+
+  val cnt2 = cnt
+    .select("CompanyReviews").distinct()
+    .orderBy(col("CompanyReviews"))
+
+  cnt2
+    .write
+    .format("json")
+    .mode(SaveMode.Overwrite)
+    .save("src/main/resources/data/test2")
 
 }
